dbe27e7bed50b43e5009dbdc90960c6a03a2d998
MoisesMed <br.moises@hotmail.com>
2021-08-24 17:11:08 -0300

    input

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,8 +2,8 @@
 import React, { useState, useEffect, useMemo } from "react";
 import InfiniteScroll from 'react-infinite-scroller';
 import {
-    Container, Button,
-    Col, Row, Table, Spinner
+    Container, Button, InputGroup, FormControl, Form,
+    Col, Row, Table, Spinner, Dropdown, DropdownButton, ButtonGroup
 } from "react-bootstrap";
 import ActionBar from "../../components/ActionBar/ActionBar"
 import moment from "moment"
@@ -16,22 +16,34 @@ import "./styles.css";
 export default function Home() {
     const [results, setResults] = useState([])
     const [list, setList] = useState(results)
-    const [inputData, setInputData] = useState(" ")
-    const [page, setPage] = useState(0)
+    const [inputData, setInputData] = useState("")
+    const [page, setPage] = useState(2)
     const [show, setShow] = useState(false)
     const [personSelected, setPersonSelected] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [orderName, setOrderName] = useState(false)
+    const [orderGender, setOrderGender] = useState(false)
+    const [qtd, setQtd] = useState([50])
+    const [nat, setNat] = useState(false)
+    const [name, setName] = useState(true)
+
 
     useEffect(() => {
-        if (inputData === "") {
-            setList(results)
-        } else {
-            setList(
-                list.filter(item => (item.name.first.toLowerCase().indexOf(inputData.toLowerCase()) > -1))
-            )
+        const handleSearch = () => {
+            if (inputData === "") {
+                setList(results)
+            } else {
+                setList(
+                    nat ?
+                        list.filter(item => (item.nat.toLowerCase().indexOf(inputData.toLowerCase()) > -1)) :
+                        list.filter(item => (item.name.first.toLowerCase().indexOf(inputData.toLowerCase()) > -1))
+                )
+            }
         }
+        handleSearch()
     }, [inputData])
 
-    const handleOrder = () => {
+    const handleOrderName = () => {
         let newList = [...results];
         newList.sort((a, b) => {
             if (a.name.first > b.name.first) {
@@ -44,36 +56,62 @@ export default function Home() {
                 }
             }
         })
-
+        setOrderName(true)
         setList(newList)
+        setOrderGender(false)
+    }
+
+    const handleOrderGender = () => {
+        let newList = [...results];
+        newList.sort((a, b) => {
+            if (a.gender > b.gender) {
+                return 1
+            } else {
+                if (b.gender > a.gender) {
+                    return -1
+                } else {
+                    return 0
+                }
+            }
+        })
+        setOrderGender(true)
+        setList(newList)
+        setOrderName(false)
     }
 
     useEffect(() => {
         const getData = async () => {
+            setIsLoading(true)
             try {
-                const response = await api.get("?results=10&seed=abcde")
+                const response = await api.get(`?results=${qtd}&seed=abcde`)
                 setResults(response.data.results)
                 setList(response.data.results)
             } catch (erro) {
                 console.log(erro)
+            } finally {
+                setIsLoading(false)
             }
         }
         getData()
+        setOrderGender(false)
+        setOrderName(false)
     }, [])
 
     const moreData = async () => {
+        setIsLoading(true)
         try {
-            const response = await api.get(`?results=10&seed=abcde&page=${page}`)
+            const response = await api.get(`?results=${qtd}&seed=abcde&page=${page}`)
             const newArray = [...results, ...response.data.results]
             setResults(newArray)
             setList(newArray)
         } catch (erro) {
             console.log(erro)
         } finally {
+            setIsLoading(false)
             setPage(page + 1)
+            setInputData("")
         }
     }
-
     const showModal = (item) => {
         setShow(true);
         setPersonSelected(item);
@@ -83,34 +121,81 @@ export default function Home() {
         setShow(false)
     }
 
+    const changeChecked = () => {
+        setName(!name)
+        setNat(!nat)
+    }
+
     return (
         <Container className="mainContainer">
             <br />
-            <text>texto aqui</text>
+            <text style={{ width: "60%", textAlign: "center", fontFamily: "Roboto" }}>
+                We at Pharma Inc, always thinking of offering our best services, developed this website to make the management and visualization of our patients' information more efficient and streamline our service.
+            </text>
+            <text style={{ width: "50%", textAlign: "center", fontFamily: "Roboto", marginTop: 8 }}>
+                In the list below, you can search for patients by name and nationality. You can also sort the NAME table alphabetically.
+            </text>
             <br />
-            <div>
-                <input
-                    className="form-input"
-                    onChange={(e) => setInputData(e.target.value)}
-                    id="input-table"
-                    placeholder="Ache um Usuario"
-                />
-                <button>Name</button>
-                <button>Nationality</button>
-            </div>
+            <Row>
+                <Col className="colInputSearch">
+                    <input
+                        className="form-input-search"
+                        value={inputData}
+                        onChange={(e) => setInputData(e.target.value)}
+                        id="input-table"
+                        placeholder="Search for name or nationality"
+                    />
+                </Col>
+                <Col>
+                    <Row style={{ justifyContent: "center" }}>
+                        Name
+                    </Row>
+                    <Row style={{ textAlign: "center" }}>
+                        <Form.Check
+                            inline
+                            type={"checkbox"}
+                            value={name}
+                            checked={name}
+                            onClick={() => changeChecked()}
+                        />
+                    </Row>
+                </Col>
+                <Col>
+                    <Row style={{ justifyContent: "center" }}>
+                        Nationality
+                    </Row>
+                    <Row style={{ textAlign: "center" }}>
+                        <Form.Check
+                            inline
+                            type={"checkbox"}
+                            value={nat}
+                            checked={nat}
+                            onClick={() => changeChecked()}
+                        />
+                    </Row>
+                </Col>
+            </Row>
+            <br />
+            <DropdownButton as={ButtonGroup} title="Quantity for Load " id="bg-vertical-dropdown-1" variant="secondary">
+                <Dropdown.Item onClick={() => setQtd(10)} eventKey="1">10</Dropdown.Item>
+                <Dropdown.Item onClick={() => setQtd(25)} eventKey="2">25</Dropdown.Item>
+                <Dropdown.Item onClick={() => setQtd(50)} eventKey="3">50</Dropdown.Item>
+            </DropdownButton>
             <br />
             <InfiniteScroll
                 pageStart={page}
                 hasMore={true || false}
                 loader={
-                    <button onClick={() => { moreData() }} style={{ display: "contents" }}>
-                        <div className="divSpinner">
+                    <div className="divSpinner">
+                        {isLoading ?
                             <Spinner animation="border" role="status">
                                 <span className="visually-hidden">Loading more...</span>
-                            </Spinner>
-                            <text style={{ margin: 12 }}>Load more...</text>
-                        </div>
-                    </button>
+                            </Spinner> : null}
+
+                        <button onClick={() => { moreData() }} style={{ display: "contents" }}>
+                            <text style={{ margin: 12 }}>Click to load more !</text>
+                        </button>
+                    </div>
                 }
             >
                 <Table striped hover style={{
@@ -120,10 +205,18 @@ export default function Home() {
                     <thead>
                         <tr>
                             <th style={{ fontFamily: "Roboto", borderTop: 0 }}>
-                                Name <button onClick={() => handleOrder()} style={{ display: "contents" }}><img src="https://img.icons8.com/material-outlined/24/000000/alphabetical-sorting.png" /></button>
+                                <button onClick={() => handleOrderName()} style={{ display: "contents", fontWeight: 700 }}>Name</button>
+                                {orderName ?
+                                    <img src="https://img.icons8.com/material-outlined/24/000000/alphabetical-sorting.png" />
+                                    : null}
                             </th>
                             <th style={{ fontFamily: "Roboto", borderTop: 0 }}>Nationality</th>
-                            <th style={{ fontFamily: "Roboto", borderTop: 0 }}>Gender</th>
+                            <th style={{ fontFamily: "Roboto", borderTop: 0 }}>
+                                <button onClick={() => handleOrderGender()} style={{ display: "contents", fontWeight: 700 }}>Gender</button>
+                                {orderGender ?
+                                    <img src="https://img.icons8.com/material-outlined/24/000000/alphabetical-sorting.png" />
+                                    : null}
+                            </th>
                             <th style={{ fontFamily: "Roboto", borderTop: 0 }}>Birth Date</th>
                             <th style={{ fontFamily: "Roboto", borderTop: 0 }}>Actions</th>
                         </tr>
@@ -145,6 +238,6 @@ export default function Home() {
 
                 </Table>
             </InfiniteScroll>
-        </Container>
+        </Container >
     )
 }
